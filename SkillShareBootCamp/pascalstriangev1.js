096724be130cdb0881ff62548a249cc478e3ee1a
Wynterwolf <shinyguns@gmail.com>
2019-05-27 01:23:59 -0500

    updating five card

--- SkillShareBootCamp/pascalstriangev1.js
+++ SkillShareBootCamp/pascalstriangev1.js
@@ -4,22 +4,22 @@ var LIMIT = 16;
 
 //The triangel will be an array of rows.
 //Each row will be an array
-var triangle = [];
+var Triangle = [];
 
 //Fill in the triangle using the well-known formula.
 for (var row = 0; row < LIMIT; row += 1) {
-  triangle.push([]);
+  Triangle.push([]);
   for (var column = 0; column <= row; column += 1) {
     if(column == 0 || column == row) {
-      triangle[row][column] = 1;
+      Triangle[row][column] = 1;
     } else {
-        triangle [row][column] = triangle[row-1][column-1] + triangle[row-1][column];
+        Triangle [row][column] = Triangle[row-1][column-1] + Triangle[row-1][column];
 
     }
   }
 }
 
 //display the data, row by row
-for (var row = 0; row < trinagle.length; row += 1) {
-  console.log(triangle[row].join(' '));
+for (var row = 0; row < Triangle.length; row += 1) {
+  console.log(Triangle[row].join(' '));
 }

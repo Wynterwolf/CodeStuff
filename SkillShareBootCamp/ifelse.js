91e2caaff2c7f8099f6f1e6bd8ed3c45a5197139
Wynterwolf <shinyguns@gmail.com>
2019-05-26 21:31:17 -0500

    updated ifelse notes

---
+++ SkillShareBootCamp/ifelse.js
@@ -0,0 +1,20 @@
+var foo = 10;
+//condition logical
+foo == 10;
+//foo == 20;
+//reurns boolean
+//if statements function
+//if (foo == 10) {
+//  console.log('hello');
+//}
+//returns undefined
+//because return value console.log is undefined
+//change code to
+if (foo == 13) {
+  console.log('hello!');
+
+} else if (foo == 10) {
+  console.log('this stuff');
+} else {
+  console.log('goodbye');
+}

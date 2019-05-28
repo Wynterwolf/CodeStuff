1a85f1f764a696575a05a9d7496abd672473f016
Wynterwolf <shinyguns@gmail.com>
2019-05-27 22:10:58 -0500

    three files updated

---
+++ SkillShareBootCamp/methodsAndThis.js
@@ -0,0 +1,13 @@
+//methods and the 'this' Keyword
+//methods are functions that are members of objects
+//objects have properties
+//example
+
+var coder = { //object beginning
+  username: 'Samantha' //property
+  years: 3 //property
+  languages: ['JS', 'ruby', 'scala']//can put in array in object
+
+  //objects in objects
+
+};//object end
